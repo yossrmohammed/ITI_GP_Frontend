6bf5c008d449d0b8d3c9365f9267fa8392823f17
yossrmohammed <yossr.khatab5@gmail.com>
2024-06-22 16:54:32 +0300

    create Hospital page

---
+++ src/pages/Hospital/HospitalICUs.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getHospitalICUs } from '../../store/slices/HospitalSlice';
+import ICUCard from '../../components/HospitalComponents/ICUCard';
+import { Link } from 'react-router-dom';
+import AddICUModal from '../../components/HospitalComponents/AddICUModel';
+import { addICU } from '../../store/slices/HospitalSlice';
+
+export default function HospitalICUs() {
+    const dispatch = useDispatch();
+    const { ICUs, isLoading } = useSelector((state) => state.ICUs);
+    const [hospitalId, setHospitalId] = useState(1);
+    const [showModal, setShowModal] = useState(false);
+
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        dispatch(getHospitalICUs(hospitalId));
+    }, [dispatch, hospitalId]);
+
+    useEffect(() => {
+        console.log("ICUs data:", ICUs);
+    }, [ICUs]);
+
+    const handleAddICU = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        
+    };
+
+
+
+
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold mb-4">Intensive Care Units</h1>
+            <div className="mb-4">
+                <Link to={'/application'} className="btn btn-secondary mr-2">
+                    Get All Applications
+                </Link>
+                <button onClick={handleAddICU} className="btn btn-primary">
+                    Add New ICU
+                </button>
+            </div>
+            {isLoading ? (
+                <div className="flex justify-center items-center">
+                    <span className="loading loading-dots loading-lg"></span>
+                </div>
+            ) : (
+                ICUs.map((icu, index) => (
+                    <ICUCard key={index} icu={icu} />
+                ))
+            )}
+            <AddICUModal
+                showModal={showModal}
+                handleCloseModal={handleCloseModal}
+               hospitalId={hospitalId}
+              
+                errors={errors}
+            />
+        </div>
+    );
+}
