642a2470442a2708e5b4c99bff854eea8ac09c7d
yossrmohammed <yossr.khatab5@gmail.com>
2024-06-22 16:51:02 +0300

    create hospital slice

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import ICUSlice from "./slices/ICUSlice";
 import ApplicationSlice from "./slices/ApplicationSlice";
+import HospitalSlice from "./slices/HospitalSlice";
 export default configureStore({
     reducer: {
         ICUs: ICUSlice,
-        applications: ApplicationSlice
+        applications: ApplicationSlice,
+        hospitals: HospitalSlice
+
+
     }
 });
