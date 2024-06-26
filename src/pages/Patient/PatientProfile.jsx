d6f9554b72c0b9e7bd1df924d02e36643042d60d
SeifOSallam <seif.osallam@gmail.com>
2024-06-27 00:03:25 +0300

    changed profile to update history

--- src/pages/Patient/PatientProfile.jsx
+++ src/pages/Patient/PatientProfile.jsx
@@ -39,7 +39,6 @@ export default function PatientProfile() {
             name: user.name,
             email: user.email,
             history: user.history,
-            gender: user.gender,
             birth_date: user.birth_date,
             phone: user.phone,
           }}
@@ -51,6 +50,7 @@ export default function PatientProfile() {
               .matches(/^[0-9]+$/, "Must be only digits")
               .min(7, 'Must be more than 7 digits')
               .max(15, 'Must be less than 15 digits'),
+            history: Yup.string().required('Required'),
           })}
           onSubmit={(values, { setSubmitting }) => {
             handleFormSubmit(values);
@@ -68,8 +68,8 @@ export default function PatientProfile() {
               <ErrorMessage name="name" component="div" className="text-red-600" />
 
               <div className="flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-4 h-4 opacity-70">
-                  <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 opacity-70">
+                  <path d="M19 2h-1V1a1 1 0 1 0-2 0v1H8V1a1 1 0 1 0-2 0v1H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zm1 18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8h16v12zM5 6V5a1 1 0 0 1 1-1h1v1a1 1 0 1 0 2 0V4h6v1a1 1 0 1 0 2 0V4h1a1 1 0 0 1 1 1v1H5z"/>
                 </svg>
                 <Field name="phone" type="text" className="input input-bordered flex-1" placeholder="Phone" />
               </div>
@@ -77,18 +77,20 @@ export default function PatientProfile() {
 
               <div className="flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 opacity-70">
-                  <path d="M19 2h-1V1a1 1 0 1 0-2 0v1H8V1a1 1 0 1 0-2 0v1H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zm1 18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8h16v12zM5 6V5a1 1 0 0 1 1-1h1v1a1 1 0 1 0 2 0V4h6v1a1 1 0 1 0 2 0V4h1a1 1 0 0 1 1 1v1H5z"/>
+                  <path d="M4 7h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2zm0 2v10h16V9H4z"/>
+                  <path d="M9 4h6v2H9z"/>
                 </svg>
                 <Field name="birth_date" type="date" className="input input-bordered flex-1" placeholder="Birth Date" />
               </div>
               <ErrorMessage name="birth_date" component="div" className="text-red-600" />
 
-              <Field as="select" name="gender" className="select select-bordered flex-1">
-                <option value="" disabled>Select Gender</option>
-                <option value="m">Male</option>
-                <option value="f">Female</option>
-              </Field>
-              <ErrorMessage name="gender" component="div" className="text-red-600" />
+              <div className="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 opacity-70">
+                  <path d="M4 19h16v2H4zm0-4h16v2H4zm0-4h16v2H4zm0-4h16v2H4zm0-4h16v2H4z"/>
+                </svg>
+                <Field as="textarea" name="history" className="textarea textarea-bordered flex-1" placeholder="Medical History" />
+              </div>
+              <ErrorMessage name="history" component="div" className="text-red-600" />
 
               <button type="submit" className="btn btn-info w-full mt-4" disabled={isSubmitting}>Update</button>
             </Form>
